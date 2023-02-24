db2e90d9d40c15f2dce7b3ba4f42727a48c34708
RowanLima <122839288+RowanLima@users.noreply.github.com>
2023-02-23 22:54:48 -0300

    Add files via upload

---
+++ Exercicio Usando entrada de dados-input.py
@@ -0,0 +1,10 @@
+codigo = int(input("Digite o código do funcionario "))
+nome = input("Digite o nome do funcionário ")
+salario = float(input("Digite o salário "))
+ativo = True
+
+
+print("Código: %d " % codigo)
+print("Nome: %s " % nome)
+print("Salário: %.2f " % salario)
+print("Ativo: %r " % ativo)
